7e1f0c1f27b6848d4d839476adf4c714441a0e21
Gridy <reality023@naver.com>
2022-06-28 11:22:13 +0900

    소셜 로그인 테스트용

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -1,7 +1,30 @@
-function Social () {
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+function Social() {
   return (
-    <div>Social</div>
-  )
+    <div>
+      <GoogleLoginButton />
+    </div>
+  );
 }
 
 export default Social;
+
+export function GoogleLoginButton() {
+  const clientId =
+    "92962841385-9crqu6ceboe2l99frk9b5jfvols875a1.apps.googleusercontent.com";
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </GoogleOAuthProvider>
+  );
+}
