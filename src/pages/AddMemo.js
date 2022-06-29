efd393302fc786a5481cc15b312e18bcd09d2407
jrl103 <jrl103@naver.com>
2022-06-29 18:11:37 +0900

    ADD : ADDMEMO 추가 기능 구현

---
+++ src/pages/AddMemo.js
@@ -0,0 +1,45 @@
+import { React, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addDataDB } from '../redux/modules/postSlice';
+
+const AddMemo = () => {
+  const dispatch = useDispatch();
+  const titletRef = useRef();
+  const contentRef = useRef();
+  const statusRef = useRef();
+  const boardTypeRef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      title: titletRef.current.value,
+      content: contentRef.current.value,
+      status: statusRef.current.value,
+      boardType: boardTypeRef.current.value
+    }
+
+    dispatch(addDataDB(data));
+    titletRef.current.value = "";
+    contentRef.current.value = "";
+    statusRef.current.value = "";
+    boardTypeRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" ref={titletRef}/>
+      <input type="text" ref={contentRef}/>
+      <select ref={statusRef}>
+        <option value="PUBLIC">PUBLIC</option>
+        <option value="PRIVATE">PRIVATE</option>
+      </select>
+      <select ref={boardTypeRef}>
+        <option value="MEMO">MEMO</option>
+      </select>    
+      <button>추가</button>
+    </form>
+  );
+};
+
+export default AddMemo;
--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -1,17 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL:"http://localhost:5005"
-})
+import {instance} from "../../shared/axios"
 
 export const getDataDB = () => {
   return async (dispatch) => {
     try {
-      const response = await instance.get("/post");
+      const response = await instance.get("/board");
       dispatch(setData(response.data));
     } catch (err) {
       console.log(err);
+    }
+  };
+};
+
+export const addDataDB = (data) => {
+  return async (dispatch) => {
+
+    try {
+      const response = await instance.post("/board", data);
+      console.log(response)
+      dispatch(addData(response.data));
+    } catch (err) {
+      console.log(err);
+      window.alert(err.response.data.message);
     }
   };
 };
@@ -26,8 +36,11 @@ const postSlice = createSlice ({
   setData: (state, action) => {
     state.list = action.payload;
   },
+  addData: (state, action) => {
+    state.list.push(action.payload);
+  },
  }
 });
 
-export const {setData}  = postSlice.actions;
+export const {setData, addData}  = postSlice.actions;
 export default postSlice.reducer;
