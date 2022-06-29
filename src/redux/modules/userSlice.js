bd99fe18476643e98dec8bc8e63673105fcf8960
munilin <munilin@naver.com>
2022-06-29 16:32:00 +0900

    Feature/Login, Register getToken save #2 #3

--- src/redux/modules/userSlice.js
+++ src/redux/modules/userSlice.js
@@ -1,13 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import axios from "axios";
+import { instance } from "../../shared/axios";
+
+import { setToken } from "../../shared/localStorage";
 
 const userSlice = createSlice({
   name: "user",
-  initialState: { list: [], isLogin: false },
+  initialState: { list: [] },
   reducers: {
     checkLogin: (state, action) => {
-      state.isLogin = action.payload;
+      state.list.push(action.payload);
     },
     createUser: (state, action) => {
       state.list.push(action.payload);
@@ -18,13 +20,17 @@ const userSlice = createSlice({
 export const loginA = (username, password) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get("http://localhost:5001/login", {
+      const response = await instance.post("/user/login", {
         username: username,
         password: password,
       });
+      dispatch(checkLogin(username, password));
+      setToken(response.data.accessToken, response.data.refreshToken);
+      alert("로그인 되었습니다");
+      window.location.replace("/");
       console.log(response);
-      dispatch(checkLogin(response));
     } catch (error) {
+      alert("아이디와 비밀번호를 확인해주세요");
       console.log(error);
     }
   };
@@ -33,14 +39,15 @@ export const loginA = (username, password) => {
 export const registerA = (username, nickname, password) => {
   return async function (dispatch) {
     try {
-      const response = await axios.post("http://localhost:5001/signup", {
+      const response = await instance.post("/user/signup", {
         username: username,
         nickname: nickname,
         imgPath: null,
         password: password,
       });
-      console.log(response);
       dispatch(createUser(username, nickname, password));
+      alert(response.data.errorMsg);
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
