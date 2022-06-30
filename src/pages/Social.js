0d6218e06481981b724b75599671b3093c19e7e9
Gridy <reality023@naver.com>
2022-06-30 18:20:36 +0900

    소셜 로그인

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -3,12 +3,14 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { removeToken, setToken } from '../shared/localStorage';
 import { instance } from '../shared/axios';
 import axios from "axios";
+import {useRef} from 'react';
+
 
 function Social () {
   const clientId = process.env.REACT_APP_GOOGLE_SOCIAL_CLIENT_ID;
   return (
     <div>
-      {/* <GoogleLoginButton /> */}
+      <GoogleLoginButton />
 
       <GoogleOAuthProvider clientId={clientId}>
         <CustomGoogleLoginButton />
@@ -53,32 +55,32 @@ function CustomGoogleLoginButton () {
   )
 }
 
-// function GoogleLoginButton () {
-//   const clientId = process.env.REACT_APP_GOOGLE_SOCIAL_CLIENT_ID;
+function GoogleLoginButton () {
+  const clientId = process.env.REACT_APP_GOOGLE_SOCIAL_CLIENT_ID;
 
-//   const success = async (data) => {
-//     try {
-//       const response = await instance.post("/user/social", {}, { headers : { Credential: data.credential }});
-//       const token = response.data;
-//       setToken(token.accessToken, token.refreshToken);
-//       console.log("로그인 성공");
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
+  const success = async (data) => {
+    try {
+      const response = await instance.post("/user/social", {}, { headers : { Credential: data.credential }});
+      const token = response.data;
+      setToken(token.accessToken, token.refreshToken);
+      console.log("로그인 성공");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-//   const error = (data) => {
-//     console.log('Login Failed');
-//   }
+  const error = (data) => {
+    console.log('Login Failed');
+  }
 
-//   return (
-//     <GoogleOAuthProvider clientId={clientId}>
-//       <GoogleLogin
-//         onSuccess={success}
-//         onError={error}
-//       />
-//     </GoogleOAuthProvider>
-//   );
-// }
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={success}
+        onError={error}
+      />
+    </GoogleOAuthProvider>
+  );
+}
 
 export default Social;
